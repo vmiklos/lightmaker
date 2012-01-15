7c9ef036a338e6940d789c5e4eed41c91b95374d
Miklos Vajna <vmiklos@frugalware.org>
2012-01-15 21:55:05 +0100

    brightness

--- src/hu/vmiklos/lightmaker/LightMakerActivity.java
+++ src/hu/vmiklos/lightmaker/LightMakerActivity.java
@@ -7,11 +7,17 @@ import android.view.WindowManager;
 import android.widget.TextView;
 
 public class LightMakerActivity extends Activity {
+	@Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         // fullscreen
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // brightness
+        WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
+        layoutParams.screenBrightness = 1;
+
         TextView tv = new TextView(this);
         // argb white
         tv.setBackgroundColor(0xffffffff);
