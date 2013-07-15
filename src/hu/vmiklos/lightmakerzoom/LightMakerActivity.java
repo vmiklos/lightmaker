c57b632d3e456a5460535ece62de561a4c2a785d
Miklos Vajna <vmiklos@suse.cz>
2013-07-15 10:31:19 +0200

    experiment with pinch zoom

--- src/hu/vmiklos/lightmakerzoom/LightMakerActivity.java
+++ src/hu/vmiklos/lightmakerzoom/LightMakerActivity.java
@@ -29,10 +29,16 @@ package hu.vmiklos.lightmakerzoom;
 
 import hu.vmiklos.lightmakerzoom.R;
 import android.app.Activity;
+import android.graphics.Matrix;
+import android.graphics.PointF;
 import android.os.Bundle;
+import android.util.FloatMath;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
 import android.widget.ViewFlipper;
 
 public class LightMakerActivity extends Activity {
@@ -42,18 +48,138 @@ public class LightMakerActivity extends Activity {
 		@Override
 		public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY)
 		{
+			if (m_scaleDetector.inZoom())
+				return false;
 			if (event1.getX() - event2.getX() > 120)
 			{
+				m_scaleDetector.reset();
 				m_viewFlipper.showNext();
 				return true;
 			}
 			else if (event2.getX() - event1.getX() > 120)
 			{
+				m_scaleDetector.reset();
 				m_viewFlipper.showPrevious();
 				return true;
 			}
 			return false;
 		}
+	}
+	
+	class ScaleListener implements OnTouchListener
+	{
+		public ScaleListener()
+		{
+			reset();
+		}
+		
+		public boolean onTouch(View v, MotionEvent event)
+		{
+			ImageView view = (ImageView) v;
+			if (view.getScaleType() == ScaleType.FIT_CENTER)
+			{
+				origValues = new float[9];
+				view.getImageMatrix().getValues(origValues);
+				matrix.setValues(origValues);
+				view.setScaleType(ScaleType.MATRIX);
+			}
+
+			switch (event.getAction() & MotionEvent.ACTION_MASK)
+			{
+			case MotionEvent.ACTION_DOWN:
+				if (inZoom())
+				{
+					savedMatrix.set(matrix);
+					start.set(event.getX(), event.getY());
+					mode = DRAG;
+				}
+				break;
+			case MotionEvent.ACTION_POINTER_DOWN:
+				oldDist = spacing(event);
+				if (oldDist > 10f) {
+					savedMatrix.set(matrix);
+					midPoint(mid, event);
+					mode = ZOOM;
+				}
+				break;
+			case MotionEvent.ACTION_UP:
+			case MotionEvent.ACTION_POINTER_UP:
+				mode = NONE;
+				float[] values = new float[9];
+				view.getImageMatrix().getValues(values);
+				currentScaleX = values[Matrix.MSCALE_X];
+				currentScaleY = values[Matrix.MSCALE_Y];
+				if (currentScaleX < origValues[Matrix.MSCALE_X])
+					matrix.setValues(origValues);
+				break;
+			case MotionEvent.ACTION_MOVE:
+				if (mode == DRAG) {
+					matrix.set(savedMatrix);
+					matrix.postTranslate(event.getX() - start.x, event.getY() - start.y);
+				}
+				else if (mode == ZOOM) {
+					float newDist = spacing(event);
+					if (newDist > 10f) {
+						matrix.set(savedMatrix);
+						float scale = newDist / oldDist;
+						matrix.postScale(scale, scale, mid.x, mid.y);
+					}
+				}
+				break;
+			}
+
+			view.setImageMatrix(matrix);
+			return true;
+		}
+
+		private float spacing(MotionEvent event)
+		{
+			float x = event.getX(0) - event.getX(1);
+			float y = event.getY(0) - event.getY(1);
+			return FloatMath.sqrt(x * x + y * y);
+		}
+
+		private void midPoint(PointF point, MotionEvent event)
+		{
+			float x = event.getX(0) + event.getX(1);
+			float y = event.getY(0) + event.getY(1);
+			point.set(x / 2, y / 2);
+		}
+		
+		public void reset()
+		{
+			((ImageView)m_viewFlipper.getCurrentView()).setScaleType(ScaleType.FIT_CENTER);
+			matrix = new Matrix();
+			savedMatrix = new Matrix();
+			mode = NONE;
+			start = new PointF();
+			mid = new PointF();
+			oldDist = 1f;
+			origValues = null;
+			currentScaleX = 0;
+			currentScaleY = 0;
+		}
+		
+		public boolean inZoom()
+		{
+			return origValues != null && origValues[Matrix.MSCALE_X] < currentScaleX;
+		}
+		
+		Matrix matrix; // Matrix of the current view
+		Matrix savedMatrix; // Matrix when the user started the touch.
+
+		static final int NONE = 0;
+		static final int DRAG = 1;
+		static final int ZOOM = 2;
+		int mode;
+
+		PointF start;
+		PointF mid;
+		float oldDist;
+
+		float[] origValues;
+		float currentScaleX;
+		float currentScaleY;
 	}
 	
 	@Override
@@ -68,14 +194,20 @@ public class LightMakerActivity extends Activity {
         m_viewFlipper.addView(imageView2);
         setContentView(m_viewFlipper);
         m_gestureDetector = new GestureDetector(this, new GestureListener());
+        m_scaleDetector = new ScaleListener();
     }
 	
 	@Override
 	public boolean onTouchEvent(MotionEvent event)
 	{
-		return m_gestureDetector.onTouchEvent(event);
+		if (m_gestureDetector.onTouchEvent(event))
+			return true;
+		if (m_scaleDetector.onTouch(m_viewFlipper.getCurrentView(), event))
+			return true;
+		return false;
 	}
 	
 	public ViewFlipper m_viewFlipper;
 	private GestureDetector m_gestureDetector;
+	public ScaleListener m_scaleDetector;
 }
